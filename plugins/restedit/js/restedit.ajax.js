f4e92461d9dff39470908b1cd938184ccf29e055
pedmands <pedmands@gmail.com>
2020-04-12 21:35:35 -0400

    Localized the root URL, nonce, and current post ID for js

--- plugins/restedit/js/restedit.ajax.js
+++ plugins/restedit/js/restedit.ajax.js
@@ -1,3 +1,10 @@
 (function($) {
-    console.info("The plugin works!");
+    // // REST API root URL
+    // console.info("The REST API root URL is: ", WPsettings.root);
+    // // Nonce Value
+    // console.info("The Nonce root URL is: ", WPsettings.nonce);
+    // // Current post ID
+    // console.info("The current post ID is: ", WPsettings.current_ID);
+
+    
 })(jQuery);
