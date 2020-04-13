ff5a93c84eb4f8d408d4b55bda5a7012e543ed36
pedmands <pedmands@gmail.com>
2020-04-12 21:46:32 -0400

    Added front end functionality using jQuery

--- plugins/restedit/js/restedit.ajax.js
+++ plugins/restedit/js/restedit.ajax.js
@@ -6,5 +6,30 @@
     // // Current post ID
     // console.info("The current post ID is: ", WPsettings.current_ID);
 
+    // Get the entry tile element:
+    const $ENTRYTITLE = $('.entry-title');
     
+    // Add an edit button and a save button directly after the entry title.
+	// Hide the save button using display:none in an inline style.
+    $ENTRYTITLE.after( '<button class="edit-button edit-title">Edit title</button><button class="edit-title save" style="display: none">Save title</button>' );
+
+    $('.edit-title.edit-button').click(function(){
+        let $originalTitle = $ENTRYTITLE.text();
+        $ENTRYTITLE.toggle();
+        $ENTRYTITLE.after('<input id="title-input" type="text">');
+        document.querySelector('#title-input').value = $originalTitle;
+        $(this).toggle();
+        $('.edit-title.save').toggle();
+    });
+
+    $('.save').click(function(){
+        let newTitle = document.querySelector('#title-input').value;
+        $ENTRYTITLE.text(newTitle);
+        $ENTRYTITLE.toggle();
+        $('#title-input').toggle();
+        $('.edit-title.edit-button').toggle();
+        $(this).toggle();
+
+    });
+
 })(jQuery);
