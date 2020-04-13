f60f28622eeafe2a6a4a525c5966cf408fb1a075
pedmands <pedmands@gmail.com>
2020-04-13 01:57:28 -0400

    Added function with ajax POST request using JWT token to edit post title

--- wpedit/JS/JWT.js
+++ wpedit/JS/JWT.js
@@ -5,6 +5,53 @@
 	const $LOGIN = $('#loginform');
 	const $LOGOUT = $('#logout');
 
+	function runAjax(postID,newTitle) {
+		$.ajax({
+			url : RESTROOT + '/wp/v2/posts/' + postID,
+			method : 'POST',
+			beforeSend: function(xhr) {
+				xhr.setRequestHeader( 'Authorization', 'Bearer ' + sessionStorage.getItem('jwtToken') );
+			},
+			data: {
+				'title' : newTitle
+			}
+		})
+
+		.done(function(response){
+			console.info(response);
+			$('#title-input').toggle();
+			$ENTRYTITLE.text(newTitle);
+			$ENTRYTITLE.toggle();
+			$('.navigation-list a[data-id="' + postID + '"]').text(newTitle);
+			$('.edit-title.edit-button').toggle();
+			$('.edit-title.save').toggle();
+
+		})
+	}
+
+	// Add edit post functionality:
+	function editPost() {
+
+	    $ENTRYTITLE.after( '<button class="edit-button edit-title">Edit title</button><button class="edit-title save" style="display: none">Save title</button>' );
+
+	    $('.edit-title.edit-button').click(function(){
+	        let $originalTitle = $ENTRYTITLE.text();
+	        $ENTRYTITLE.toggle();
+	        $ENTRYTITLE.after('<input id="title-input" type="text">');
+	        document.querySelector('#title-input').value = $originalTitle;
+	        $(this).toggle();
+	        $('.edit-title.save').toggle();
+	    });
+
+	    $('.save').click(function(){
+			let postID = document.querySelector('.post').getAttribute('data-id');
+			let newTitle = document.querySelector('#title-input').value;
+			runAjax(postID,newTitle);
+	    });
+
+	}
+
+	// Get JWT token
 	function getToken(username,password) {
 
 		$.ajax({
@@ -20,6 +67,7 @@
 			sessionStorage.setItem('jwtToken', response.token);
 			$LOGIN.toggle();
 			$LOGOUT.toggle();
+			editPost();
 		})
 
 		.fail(function(response){
@@ -39,6 +87,8 @@
 		let username = document.querySelector('#user_login').value;
 		let password = document.querySelector('#user_pass').value;
 		console.info("Username: " + username + " Password: " + password);
+
+		$('.username').text(username);
 
 		// Get JWT token
 		getToken( username, password );
