d6ef352c0adacae1bc4bf2759f085853a989977d
pedmands <pedmands@gmail.com>
2020-04-12 21:00:12 -0400

    Initialized plugin for front-end editing through API (cookie)

---
+++ plugins/restedit.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Front-end editing via the REST API
+Description: Extend cookie authentication to the REST API to allow front-end editing.
+Version:     0.1
+Author:      Preston Edmands
+Author URI:  https://github.com/pedmands/WP-API-Research-II
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Domain Path: /languages
+Text Domain: restedit
+*/
+
