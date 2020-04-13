3effec6095469972f21abd86af121d4055f165e3
pedmands <pedmands@gmail.com>
2020-04-13 01:37:14 -0400

    Added functions to get JWT token on login with ajax and clear token on logout

--- wpedit/JS/JWT.js
+++ wpedit/JS/JWT.js
@@ -1,9 +1,37 @@
 (function($){
 
-	const RESTROOT = 'localhost/WP-API-2/wp-json';
+	const RESTROOT = 'http://localhost/WP-API-2/wp-json';
 	const $ENTRYTITLE = $('.post-title');
 	const $LOGIN = $('#loginform');
 	const $LOGOUT = $('#logout');
+
+	function getToken(username,password) {
+
+		$.ajax({
+			url: RESTROOT + '/jwt-auth/v1/token',
+			method: 'POST',
+			data: {
+				'username' : username,
+				'password' : password
+			}
+		})
+
+		.done(function(response){
+			sessionStorage.setItem('jwtToken', response.token);
+			$LOGIN.toggle();
+			$LOGOUT.toggle();
+		})
+
+		.fail(function(response){
+			console.error("REST error.");
+		})
+	}
+
+	function clearToken() {
+		sessionStorage.removeItem('jwtToken');
+		$LOGIN.toggle();
+		$LOGOUT.toggle();
+	}
 
 	$LOGIN.toggle();
 	$('#login_button').click(function(e){
@@ -11,6 +39,12 @@
 		let username = document.querySelector('#user_login').value;
 		let password = document.querySelector('#user_pass').value;
 		console.info("Username: " + username + " Password: " + password);
+
+		// Get JWT token
+		getToken( username, password );
 	});
+
+	// Clear token on logout
+	$('#logout').click(clearToken);
 
 })(jQuery);
